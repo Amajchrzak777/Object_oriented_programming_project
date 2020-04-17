7da75bd07133e541f3e40f8f1618421049660c68
Amajchrzak777 <amajchrzak777@gmail.com>
2020-04-17 19:56:53 +0200

    added class which draw the map for the game

--- First_project/First_project/First_project.cpp
+++ First_project/First_project/First_project.cpp
@@ -1,20 +1,55 @@
-﻿// First_project.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
+﻿// Adam Majchrzak 17.04.2020r.
+// Object_oriented_programming_project
 
 #include <iostream>
+#include <cstdlib>
+
+class Map;
+
+
+class Map {
+    int width;
+    int height;
+public:
+    void draw_the_map(); //draw the map for the game
+    void check_errors(int& x); //checking erros
+};
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Map draw;
+    draw.draw_the_map();
 }
 
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+void Map::check_errors(int& x) {
+    while ((x < 10) || (x > 70)) {
+        std::cout << "wrong scope ! try again";
+        std::cin >> x;
+    }
+}
 
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
+
+void Map::draw_the_map() {
+    std::cout << "Enter the width the scope is [10, 70]";
+    std::cin >> width;
+    check_errors(width);
+    std::cout << "Enter the height the scope is [10,70]";
+    std::cin >> height; 
+    check_errors(height);
+    system("cls");
+    for (int i = 0; i < width; ++i) {
+        std::cout << "--";
+    }
+    for (int i = 0; i < height; ++i) {
+        std::cout << std::endl <<"|";
+        for (int j = 0; j < (width - 1); ++j) {
+            std::cout << "  ";
+        }
+        std::cout << "|";
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < width; ++i) {
+        std::cout << "--";
+    }
+}
