f453fb26c0536fc9d35315f726aa2da5dab9b8cb
Amajchrzak777 <amajchrzak777@gmail.com>
2020-04-19 13:09:14 +0200

    it works let's add new classes

---
+++ First_project_testy/First_project_testy/First_project_testy.cpp
@@ -0,0 +1,17 @@
+﻿// First_project_testy.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include "includes.h"
+#include "Classes.h"
+
+
+int main()
+{
+	srand(time(NULL));
+	Map* m = new Map();
+	m->dimension_of_board();
+	m->key_to_play();
+	m->initialize_fields();
+	m->give_the_coordinates();
+	m->draw_the_map();
+}
