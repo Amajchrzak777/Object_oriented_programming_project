da402434578a728a3f38fde5bee241ec15a84953
Amajchrzak777 <amajchrzak777@gmail.com>
2020-05-15 16:28:08 +0200

    Teraz dodam abstrakcyjna klase animal po ktorej dziedziczyc beda zwierzeta oraz czlowiek, commit aby zapisac stan w ktorym program dziala

--- First_project_testy/First_project_testy/First_project_testy.cpp
+++ First_project_testy/First_project_testy/First_project_testy.cpp
@@ -9,19 +9,21 @@ class Map;
 class Organism;
 class Human;
 
-int main()
-{
-	int x1 = 5;
-	int y1 = 5;
-	const char w1 = 'W';
+Map m;
+
+void fill_the_world() {
 	srand(time(NULL));
-	Map m;
 	//m.dimension_of_board();
-	m.add_organism(new Human(m,20,3, 3, 'H'));
-	m.add_organism(new Sheep(m,15,5, 10, 'A'));
+	m.add_organism(new Human(m, 20, 3, 3, 'H'));
+	m.add_organism(new Sheep(m, 15, 5, 10, 'A'));
 	m.add_organism(new Sheep(m, 10, 7, 7, 'B'));
 	m.add_organism(new Sheep(m, 11, 1, 1, 'C'));
 	m.add_organism(new Sheep(m, 11, 10, 10, 'D'));
 	m.add_organism(new Sheep(m, 11, 6, 6, 'E'));
+}
+
+int main()
+{
+	fill_the_world();
 	m.game();
 }
